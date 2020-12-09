5f57003508c6aa0cde9f5e135c80f815ae91b3cf
Joser <josertrs@gmail.com>
2020-12-08 19:18:36 -0800

    Setting up controller

---
+++ AnimeAlarm/AnimeTitleViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AnimeTitleViewController.swift
+//  AnimeAlarm
+//
+//  Created by Jose Torres-Vargas on 12/8/20.
+//
+
+import UIKit
+
+
+
+class AnimeTitleViewController: UICollectionViewController {
+    // MARK: - Properties
+    private let reuseIdentifier = "AnimeTitleCell"
+
+    
+    private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+}
---
+++ AnimeAlarm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimeAlarm
+//
+//  Created by Jose Torres-Vargas on 12/8/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
